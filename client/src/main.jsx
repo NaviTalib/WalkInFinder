a8ccf0a522c15760d64094aa8fb1dece07609c15
NaviTalib <talibali786.ta@gmail.com>
2026-09-17 02:21:47 +0530

    console bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <App />);
+} else {
+  createRoot(rootElement).render(<App />);
+}
